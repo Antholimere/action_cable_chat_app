9d3e2f045ff3e03a443e04782fa563e039f5790f
Anthony Limere <anthonylimere@gmail.com>
2016-09-09 15:35:59 +0200

    Implement basic action-cable and authorization

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,5 +1,3 @@
-require 'test_helper'
-
 class MessagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
@@ -19,13 +17,13 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should handle invalid message" do
     login_as @user
-    post messages_url, params: { message: { content: "" } }
+    post messages_url, params: { message: { content: "" } }, xhr: true
     assert_response :success
   end
 
   test "should create valid message" do
     login_as @user
-    post messages_url, params: { message: { content: "Lorem" } }
-    assert_response :redirect
+    post messages_url, params: { message: { content: "Lorem" } }, xhr: true
+    assert_response :success
   end
 end
